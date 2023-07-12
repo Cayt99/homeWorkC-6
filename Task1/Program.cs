e0ec1989661035e19979b11383d1a8d3fcd0e32a
Usmanov Saut <cayt99@mail.ru>
2023-07-12 16:20:05 +0600

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3 
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int count = 0;
+        int m = Prompt("Введите количество чисел: ");
+
+        for (int i = 0; i < m; i++)
+        {
+            int number = Prompt($"Введите число #{i + 1}: ");
+            if (number > 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"Количество чисел, больше 0: {count}");
+    }
+
+    static int Prompt(string message)
+    {
+        Console.Write(message);
+        string value = Console.ReadLine();
+        int result;
+        while (!int.TryParse(value, out result))
+        {
+            Console.WriteLine("Некорректный ввод. Попробуйте снова.");
+            Console.Write(message);
+            value = Console.ReadLine();
+        }
+        return result;
+    }
+}
